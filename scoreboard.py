2dab955198559177493b4cdbd90380a3bb975113
prabhnoor1301 <prabh13noorkaur@gmail.com>
2024-05-04 16:54:29 +0530

    Initial commit

---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"Score :{self.score}", align="center", font=("Ariel", 24, "normal"))
+
+    def gameover(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=("Ariel", 24, "normal"))
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
